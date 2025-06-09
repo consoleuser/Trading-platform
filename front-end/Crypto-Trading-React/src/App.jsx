6806940607921f7c15c4b4b58e108ca042054c71
Lemon <yuriykotyashko7@gmail.com>
2025-06-09 13:02:53 -0400

    Front-end
    Front-end

---
+++ front-end/Crypto-Trading-React/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+import { Navbar } from './pages/Navbar/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar>Navbar</Navbar>
+    </>
+  )
+}
+
+export default App
